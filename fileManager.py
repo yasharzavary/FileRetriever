e672232aa4187565f62856b40272a510390d2afb
yasharzavary <yasharzavary@yasharzavarys-Laptop.local>
2025-02-03 00:29:17 +0330

    chore(fileManager): raw structure of file manager class created implementaion of the class will added in the near future

---
+++ fileManager.py
@@ -0,0 +1,19 @@
+
+
+
+class FileManager:
+    def __init__(self, files_address: list):
+        if not files_address: raise('file address is empty')
+        self.files_address = files_address
+
+    def addFile(self, filePath):
+        pass
+
+    def __filter(self, text):
+        pass
+
+    def __normalize(self, text):
+        pass
+    
+    def __addToBinaryDict(self):
+        pass
