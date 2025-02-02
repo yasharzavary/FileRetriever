eaf8dfdcdafc8d9e81a7278db17de1cd2e845e05
yasharzavary <yasharzavary@yasharzavarys-Laptop.local>
2025-02-03 00:17:20 +0330

    feat(preprocess): add verify language and detect langauge to class detect langauge will detect language of the file. verify language will fileter english writed files and return it for next step

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@
 
 """
 import os
+from langdetect import detect, DetectorFactory, LangDetectException
 
 class Preprocess:
     def __init__(self, specific_location: str = None) -> None:
@@ -18,8 +19,9 @@ class Preprocess:
             starting_path = 'C:\\\\' if os.name == 'nt' else '/'  # start path of the PC depend on operation system
         else:
             starting_path = self.__spec_loc
-        all_files = self._file_reader(starting_path)
-        print(all_files)
+        # read all files and filter language
+        verified_files_address = self.__verify_language(self._file_reader(starting_path))
+        print(verified_files_address)
 
     def _file_reader(self, start_director: str):
         """
@@ -34,11 +36,31 @@ class Preprocess:
                     all_files.append(file_path)
         return all_files
 
-    def __verify_language(self):
-        pass
+    def __verify_language(self, raw_files_address:list):
+        """
+            responsible for find english files and save them in one list and return them back.
+        :param raw_files_address: lisr of raw file address that there language don't known
+        :return: filtered english files adress list
+        """
+        filtered_files = list()
+        for file_address in raw_files_address:
+            # if file is english, add it to the filtered list
+            if self.__language_detect(file_address) == 'en': filtered_files.append(file_address)
+        return filtered_files
 
-    def __language_detect(self):
-        pass
 
-x = Preprocess('/Users/yasharzavary/Desktop/Projects/information reterival/Project')
+    def __language_detect(self, file_address: str):
+        """
+            this function is responsible for detecting langauge of texts and return it to the father function
+        :param file_address: file address for reading and detecting language
+        :return: if detecting complete, language, if not empty string(False)
+        """
+        with open(file_address, 'r') as file:
+            text = file.read().strip()
+            if not text: return ''  # if file empty, return False
+            # try to detect text of the file and return related result.
+            try: return detect(text)
+            except: return ''
+
+x = Preprocess('/Users/yasharzavary/Desktop/Projects/tests/information_reterival_tesets')
 x.pre_process()
