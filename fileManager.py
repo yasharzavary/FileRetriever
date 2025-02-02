b513552ecddb39497224e252fdfedf28c7edb102
yasharzavary <yasharzavary@yasharzavarys-Laptop.local>
2025-02-03 00:54:58 +0330

    feat(filemanager): filter funciton writed now we can detect usefull tokens and ignore verbs, stopwords and adjectives in next versions we will improve this part to add frequency detection too

--- fileManager.py
+++ fileManager.py
@@ -1,3 +1,4 @@
+import spacy
 
 
 
@@ -5,15 +6,24 @@ class FileManager:
     def __init__(self, files_address: list):
         if not files_address: raise('file address is empty')
         self.files_address = files_address
+        self.nlp = spacy.load('en_core_web_sm')
 
-    def addFile(self, filePath):
-        pass
+    def addFile(self):
+        for file_address in self.files_address:
+            file = open(file_address)
+            txt = file.read().strip()
+            file.close()
+            tokens = self.__filter(txt)
+            print(tokens)
 
     def __filter(self, text):
-        pass
+        words = self.nlp(text)
+        return [token.text for token in words
+                if token.is_alpha and not token.is_stop and token.pos not in ['VERB', 'AUX', 'ADJ']
+                ]
 
     def __normalize(self, text):
         pass
-    
+
     def __addToBinaryDict(self):
         pass
