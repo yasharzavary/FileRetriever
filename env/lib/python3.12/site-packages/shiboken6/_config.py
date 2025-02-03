c7e7110282a6a9a914c33e70545392f01a0008ad
yashar <115088770+yasharzavary@users.noreply.github.com>
2025-02-03 19:41:34 +0330

    chore(lfs): pyside web support add with lfs this file is higher than gihub maximum file size limit, so I use lfs to store it in repo

---
+++ env/lib/python3.12/site-packages/shiboken6/_config.py
@@ -0,0 +1,12 @@
+shiboken_library_soversion = str(6.8)
+
+version = "6.8.2"
+version_info = (6, 8, 2, "", "")
+
+__build_date__ = '2025-01-27T09:02:18+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.8.2'
+__qt_macos_min_deployment_target__ = '12'
