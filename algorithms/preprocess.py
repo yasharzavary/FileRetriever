f384d64abfe03fc254db991922aa89f2306437fe
yashar <115088770+yasharzavary@users.noreply.github.com>
2025-02-04 12:44:18 +0330

    feat(preprocess): add file counter func and relation with ui now, prerpocess first count files in one specific location(depend on what user say) and can send signals to update progress bar in the ui.

--- algorithms/preprocess.py
+++ algorithms/preprocess.py
@@ -10,44 +10,85 @@ class Preprocess:
         if specific_location is None: self.__spec_loc = 'all'
         else: self.__spec_loc = specific_location
 
+    @property
+    def loc(self):
+        return self.__spec_loc
+    @loc.setter
+    def loc(self, new):
+        self.__spec_loc = new
 
-    def pre_process(self):
+    def fileCount(self, dir, callback):
+        """
+            count number of file in the system or location
+        :param dir: directory that user want to read files
+        :param callback: callback function for talking with ui
+        :return:
+        """
+        i = 0
+        tot = 0
+        for _, _, files in os.walk(dir):
+            i+=0.0001
+            callback(i)
+            tot += len(files)
+
+        self.__fileNumber = tot
+
+    def pre_process(self, callback, phase):
         """
             this will run in the start of the app and control all parts of the preprocess
+        :param callback: callback function for talking with ui
+        :param phase: phase that this function will do depend on specific state ui say
         :return: None
         """
         if self.__spec_loc == 'all':
             starting_path = 'C:\\\\' if os.name == 'nt' else '/'  # start path of the PC depend on operation system
         else:
             starting_path = self.__spec_loc
-        # read all files and filter language
-        verified_files_address = self.__verify_language(self._file_reader(starting_path))
-        FileManager(verified_files_address).addFile()
+        # read all files and filter language(depend on what state we are on ui)
+        if phase == 'count':
+            self.fileCount(starting_path, callback)
+        elif phase == 'read':
+            self.__verified_files_address = self._file_reader(starting_path, callback)
+        elif phase == 'lanVerify':
+            self.__verified_files_address, self.__fileNumber = self.__verify_language(self.__verified_files_address, callback)
+        elif phase == 'analyze':
+            FileManager(self.__verified_files_address).addFile(callback)
 
-    def _file_reader(self, start_director: str):
+    def _file_reader(self, start_director: str, callback):
         """
             this function wil read all .txt files from the computer.
         :return: list of all .txt files.
         """
         all_files = list()  # all files list
+        i = 0  # process bar show
+        step = 100 / self.__fileNumber  # process bar steps
         for root, dirs, files in os.walk(start_director):  # walk in each location of PC
             for file in files:  # for each file in eac location
+                i += step
+                callback(i)  # update process bar in ui
                 if file.endswith('.txt'):  # add it to the all files list if the file is txt file.
                     file_path = os.path.join(root, file)
                     all_files.append(file_path)
         return all_files
 
-    def __verify_language(self, raw_files_address:list):
+    def __verify_language(self, raw_files_address:list, callback):
         """
             responsible for find english files and save them in one list and return them back.
         :param raw_files_address: lisr of raw file address that there language don't known
         :return: filtered english files adress list
         """
+        verifyFileNumber = 0
+        i = 0
+        step = 100 / len(raw_files_address)
         filtered_files = list()
         for file_address in raw_files_address:
+            i+=step
+            callback(i)
             # if file is english, add it to the filtered list
-            if self.__language_detect(file_address) == 'en': filtered_files.append(file_address)
-        return filtered_files
+            if self.__language_detect(file_address) == 'en':
+                filtered_files.append(file_address)
+                verifyFileNumber+=1
+        return filtered_files, verifyFileNumber
 
 
     def __language_detect(self, file_address: str):
@@ -56,12 +97,12 @@ class Preprocess:
         :param file_address: file address for reading and detecting language
         :return: if detecting complete, language, if not empty string(False)
         """
-        with open(file_address, 'r') as file:
-            text = file.read().strip()
-            if not text: return ''  # if file empty, return False
-            # try to detect text of the file and return related result.
-            try: return detect(text)
-            except: return ''
+        try:
+            with open(file_address, 'r') as file:
+                    text = file.read().strip()
+                    if not text: return ''  # if file empty, return False
+                    # try to detect text of the file and return related result.
+                    return detect(text)
+        except: return ''
 
-x = Preprocess('/Users/yasharzavary/Desktop/Projects/tests/information_reterival_tesets')
-x.pre_process()
+
